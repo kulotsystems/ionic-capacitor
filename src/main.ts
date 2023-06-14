45830eafe05e777c867c7f82fa3703ad510437ae
kulotsystems <kulotsystems@gmail.com>
2023-06-14 22:21:46 +0800

    Use Camera.

--- src/main.ts
+++ src/main.ts
@@ -23,10 +23,14 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* PWA */
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 const app = createApp(App)
     .use(IonicVue)
     .use(router);
 
 router.isReady().then(() => {
     app.mount('#app');
+    defineCustomElements(window).then(r => {});
 });
